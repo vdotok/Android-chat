708a1df7a618a98d5f1ed96fe32f0992ef5c9980
Fajar Chishtee <fajarchishtee@Fajars-MacBook-Pro.local>
2022-03-16 13:24:44 +0500

    network base url chnge

--- Network/src/main/java/com/vdotok/network/utils/Constants.kt
+++ Network/src/main/java/com/vdotok/network/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.vdotok.network.utils
 
 object Constants {
-    const val BASE_URL = "Enter API Service URL here"
+    const val BASE_URL = "https://Please enter Api Service URl here/"
 }
