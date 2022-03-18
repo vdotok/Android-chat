1c475d8a779685bc4eb055c54a0d5760a61b81a5
Fajar Chishtee <fajarchishtee@Fajars-MacBook-Pro.local>
2022-03-18 12:55:04 +0500

    minor change

--- Network/src/main/java/com/vdotok/network/utils/Constants.kt
+++ Network/src/main/java/com/vdotok/network/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.vdotok.network.utils
 
 object Constants {
-    const val BASE_URL = "https://Please enter Api Service URl here/"
+    const val BASE_URL = "Please Enter API Service URL"
 }
