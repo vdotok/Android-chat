d0cfa94ce68814fcb6da86abadbe82ea42f08592
Fajar Chishtee <fajarchishtee@Fajars-MacBook-Pro.local>
2022-03-18 15:42:45 +0500

    base url change

--- Network/src/main/java/com/vdotok/network/network/api/ApiService.kt
+++ Network/src/main/java/com/vdotok/network/network/api/ApiService.kt
@@ -8,33 +8,33 @@ import retrofit2.http.POST
 
 interface ApiService {
 
-    @POST("API/v0/Login")
+    @POST("/API/v0/Login")
     suspend fun loginUser(@Body model: LoginUserModel): LoginResponse
 
-    @POST("API/v0/SignUp")
+    @POST("/API/v0/SignUp")
     suspend fun signUp(@Body model: SignUpModel): LoginResponse
 
-    @POST("API/v0/CheckUsername")
+    @POST("/API/v0/CheckUsername")
     suspend fun checkUserName(@Body model: CheckUserModel): Response<LoginResponse>
 
-    @POST("API/v0/CheckEmail")
+    @POST("/API/v0/CheckEmail")
     suspend fun checkEmail(@Body model: CheckUserModel): LoginResponse
 
-    @POST("API/v0/AllUsers")
+    @POST("/API/v0/AllUsers")
     suspend fun getAllUsers(@Header("Authorization") auth_token: String): GetAllUsersResponseModel
 
-    @POST("API/v0/AllGroups")
+    @POST("/API/v0/AllGroups")
     suspend fun getAllGroups(@Header("Authorization") auth_token: String): AllGroupsResponse
 
-    @POST("API/v0/DeleteGroup")
+    @POST("/API/v0/DeleteGroup")
     suspend fun deleteGroup(@Header("Authorization") auth_token: String, @Body model: DeleteGroupModel): DeleteGroupResponseModel
 
-    @POST("API/v0/RenameGroup")
+    @POST("/API/v0/RenameGroup")
     suspend fun updateGroupName(@Header("Authorization") auth_token: String, @Body model: UpdateGroupNameModel): UpdateGroupNameResponseModel
 
-    @POST("API/v0/CreateGroup")
+    @POST("/API/v0/CreateGroup")
     suspend fun createGroup(@Header("Authorization") auth_token: String, @Body model: CreateGroupModel): CreateGroupResponse
 
-    @POST("API/v0/AuthenticateSDK")
+    @POST("/API/v0/AuthenticateSDK")
     suspend fun authSDK(@Body model: AuthenticationRequest): Response<AuthenticationResponse>
 }
